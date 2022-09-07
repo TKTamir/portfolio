77fe47a89cac59cc87a0854739ad1daa0d195bb9
TKTamir <104828119+TKTamir@users.noreply.github.com>
2022-09-07 18:08:13 +0200

    Define methods for Banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,48 @@
+import { useState, useEffect } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import { ArrowRightCircle } from 'react-bootstrap-icons';
+import headerImg from '../assets/img/header-img.svg';
 
 export const Banner = () => {
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const toRotate = ['Full-Stack Web Developer', 'Full-Stack Web Developer'];
+  const [text, setText] = useState('');
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const period = 2000;
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+
+    return () => {
+      clearInterval(ticker);
+    };
+  }, [text]);
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting
+      ? fullText.substring(0, text - 1)
+      : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta((prevDelta) => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setDelta(period);
+    } else if (isDeleting && updatedText === '') {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setDelta(500);
+    }
+  };
   return (
     <section className="banner" id="home">
       <Container>
@@ -9,17 +50,20 @@ export const Banner = () => {
           <Col xs={12} md={6} xl={7}>
             <span className="tagline">Welcome to my Portfolio</span>
             <h1>
-              {`Hi I'm webdecoded`}
-              <span className="wrap"> web developer</span>
+              {`Hi I'm webdecoded `}
+              <span className="wrap">{text}</span>
             </h1>
             <p>
               Lorem ipsum loren ipsum lorem ipsum lorem ipsum lorem ipsum Lorem ipsum loren ipsum
               lorem ipsum lorem ipsum lorem ipsum
             </p>
-            <button onClick={() => console.log('connect')}>Let's Connect</button>
+            <button onClick={() => console.log('connect')}>
+              Let's Connect
+              <ArrowRightCircle size={25} />
+            </button>
           </Col>
           <Col xs={12} md={6} xl={5}>
-            <img src={} alt="Header IMG"/>
+            <img src={headerImg} alt="Header IMG" />
           </Col>
         </Row>
       </Container>
