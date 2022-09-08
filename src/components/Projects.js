9676c84bef05b366d1a70c80996540f512c7f4ea
TKTamir <104828119+TKTamir@users.noreply.github.com>
2022-09-08 17:32:01 +0200

    Add TrackVisibility

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,8 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import { Container, Col, Row, Tab, Nav } from 'react-bootstrap';
 import { ProjectCard } from './ProjectCard';
+import TrackVisibility from 'react-on-screen';
+import 'animate.css';
 import colorSharp2 from '../assets/img/color-sharp2.png';
 import projImg1 from '../assets/img/project-img1.png';
 import projImg2 from '../assets/img/project-img2.png';
@@ -43,40 +46,47 @@ export const Projects = () => {
     <section className="project" id="project">
       <Container>
         <Row>
-          <Col>
-            <h2>Projects</h2>
-            <p>
-              Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem Lorem ipsum lorem ipsum lorem
-              ipsum lorem ipsum lorem
-            </p>
-            <Tab.Container id="project-tabs" defaultActiveKey="first">
-              <Nav
-                variant="pills"
-                className="nav-pills mb-5 justify-content-center align-items-center"
-                id="pills-tab"
-              >
-                <Nav.Item>
-                  <Nav.Link eventKey="first">Tab 1</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link eventKey="second">Tab 2</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link eventKey="third">Tab 3</Nav.Link>
-                </Nav.Item>
-              </Nav>
-              <Tab.Content>
-                <Tab.Pane eventKey="first">
-                  <Row>
-                    {projects.map((project, index) => {
-                      return <ProjectCard key={index} {...project} />;
-                    })}
-                  </Row>
-                </Tab.Pane>
-                <Tab.Pane eventKey="second">Lorem Ipsum2</Tab.Pane>
-                <Tab.Pane eventKey="third">Lorem Ipsum3</Tab.Pane>
-              </Tab.Content>
-            </Tab.Container>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) => (
+                <div className={isVisible ? 'animate__animated animate__slideInUp' : ''}>
+                  <h2>Projects</h2>
+                  <p>
+                    Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem Lorem ipsum lorem ipsum
+                    lorem ipsum lorem ipsum lorem
+                  </p>
+
+                  <Tab.Container id="project-tabs" defaultActiveKey="first">
+                    <Nav
+                      variant="pills"
+                      className="nav-pills mb-5 justify-content-center align-items-center"
+                      id="pills-tab"
+                    >
+                      <Nav.Item>
+                        <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                      </Nav.Item>
+                    </Nav>
+                    <Tab.Content>
+                      <Tab.Pane eventKey="first">
+                        <Row>
+                          {projects.map((project, index) => {
+                            return <ProjectCard key={index} {...project} />;
+                          })}
+                        </Row>
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="second">Lorem Ipsum2</Tab.Pane>
+                      <Tab.Pane eventKey="third">Lorem Ipsum3</Tab.Pane>
+                    </Tab.Content>
+                  </Tab.Container>
+                </div>
+              )}
+            </TrackVisibility>
           </Col>
         </Row>
       </Container>
