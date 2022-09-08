d268edb070fc9776eebf0202d14ef297b9f77193
TKTamir <104828119+TKTamir@users.noreply.github.com>
2022-09-08 15:35:36 +0200

    Create and setup ProjectCard.js

---
+++ src/components/ProjectCard.js
@@ -0,0 +1,15 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
+import { Col } from 'react-bootstrap';
+export const ProjectCard = ({ title, description, imgUrl }) => {
+  return (
+    <Col sm={6} md={4}>
+      <div className="proj-imgbx">
+        <img src={imgUrl} alt="Project-Image" />
+        <div ClassName="proj-txtx">
+          <h4>{title}</h4>
+          <span>{description}</span>
+        </div>
+      </div>
+    </Col>
+  );
+};
