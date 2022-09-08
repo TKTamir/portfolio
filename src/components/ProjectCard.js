cab97d7fdcac11dcb79a6544809db2895b605c3c
TKTamir <104828119+TKTamir@users.noreply.github.com>
2022-09-08 16:21:53 +0200

    Fix typo

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -5,7 +5,7 @@ export const ProjectCard = ({ title, description, imgUrl }) => {
     <Col sm={6} md={4}>
       <div className="proj-imgbx">
         <img src={imgUrl} alt="Project-Image" />
-        <div ClassName="proj-txtx">
+        <div className="proj-txtx">
           <h4>{title}</h4>
           <span>{description}</span>
         </div>
