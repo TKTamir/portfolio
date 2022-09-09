d6dc8afc24d3b5d18285096979b81f4efd3fea04
TKTamir <104828119+TKTamir@users.noreply.github.com>
2022-09-09 12:10:13 +0200

    Import and connect Contact Component

---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+export const Contact = () => {
+  return (
+    <section className="contact" id="contact">
+      <div className="contact">
+        <p>
+          Tamir Nissim Kahalany <br /> Berlin, Germany.
+        </p>
+        <a href="mailto: tamirkhy@gmail.com">tamirkhy@gmail.com</a>
+      </div>
+    </section>
+  );
+};
