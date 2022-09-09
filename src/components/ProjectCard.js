9c5321c093071dca0292cdeac888510e4ec27f2d
TKTamir <104828119+TKTamir@users.noreply.github.com>
2022-09-09 13:15:40 +0200

    Add url to project card

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,14 +1,18 @@
 /* eslint-disable jsx-a11y/img-redundant-alt */
 import { Col } from 'react-bootstrap';
-export const ProjectCard = ({ title, description, imgUrl }) => {
+export const ProjectCard = ({ title, description, imgUrl, appUrl }) => {
   return (
     <Col sm={6} md={4}>
       <div className="proj-imgbx">
         <img src={imgUrl} alt="Project-Image" />
+
         <div className="proj-txtx">
           <h4>{title}</h4>
           <span>{description}</span>
         </div>
+      </div>
+      <div className="project-url">
+        <a href={appUrl}>Open</a>
       </div>
     </Col>
   );
