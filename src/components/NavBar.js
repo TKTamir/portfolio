4b71abd045bf8c695e05d3f8c5095e6c4f6aff2a
TKTamir <104828119+TKTamir@users.noreply.github.com>
2022-09-09 12:50:07 +0200

    Add className to logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -30,7 +30,7 @@ export const NavBar = () => {
     <Navbar expand="md" className={scrolled ? 'scrolled' : ''}>
       <Container>
         <Navbar.Brand href="#home">
-          <img src={logo} alt="Logo" />
+          <img src={logo} alt="Logo" className="logo" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav">
           <span className="navbar-toggler-icon"></span>
