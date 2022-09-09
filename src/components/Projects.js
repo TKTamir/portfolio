b34e0a3614294bbd2454ed099d853749783be0fb
TKTamir <104828119+TKTamir@users.noreply.github.com>
2022-09-09 10:40:43 +0200

    Edit imports

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,41 +4,47 @@ import { ProjectCard } from './ProjectCard';
 import TrackVisibility from 'react-on-screen';
 import 'animate.css';
 import colorSharp2 from '../assets/img/color-sharp2.png';
-import projImg1 from '../assets/img/project-img1.png';
-import projImg2 from '../assets/img/project-img2.png';
-import projImg3 from '../assets/img/project-img3.png';
+import portfolioImg from '../assets/img/project-images/portfolio.png';
+import pokeImg from '../assets/img/project-images/pokedex.png';
+import apiImg from '../assets/img/project-images/api.png';
+import flixImg from '../assets/img/project-images/appformovies.png';
+import meetImg from '../assets/img/project-images/meet.png';
+import flixAngularImg from '../assets/img/project-images/angularImg.png';
 
 export const Projects = () => {
   const projects = [
     {
       title: 'Pokedex App',
-      description: 'Design & Development',
-      imgUrl: projImg1,
+      description:
+        'Portfolio website built with HTML and CSS3, the website includes a contact form and an SVG animation.',
+      imgUrl: portfolioImg,
     },
     {
       title: 'React Movies App',
-      description: 'Design & Development',
-      imgUrl: projImg2,
+      description:
+        'Pokedex app is a simple app that lists Pokémons by name and shows their data, Clicking on a Pokémon entry displays more details about the Pokémon. In the app Pokémons can be filtered by their names. All data is loaded from the PokeAPI.',
+      imgUrl: pokeImg,
     },
     {
       title: 'Chat Native App',
       description: 'Design & Development',
-      imgUrl: projImg3,
+      imgUrl: apiImg,
     },
     {
       title: 'Meet Progressive Web App',
       description: 'Design & Development',
-      imgUrl: projImg1,
+      imgUrl: flixImg,
     },
     {
       title: 'Angular Movies App',
       description: 'Design & Development',
-      imgUrl: projImg2,
+      imgUrl: meetImg,
     },
     {
       title: 'Node.js Movies API ',
-      description: 'Design & Development',
-      imgUrl: projImg3,
+      description:
+        'Server-side component of a movies web application, the API allows users to get data about movies, genres and directors as well as storing favorite movies in the database after signing up. built with Node.js, Express and MongoDB.',
+      imgUrl: flixAngularImg,
     },
   ];
 
@@ -51,7 +57,7 @@ export const Projects = () => {
               {({ isVisible }) => (
                 <div className={isVisible ? 'animate__animated animate__slideInUp' : ''}>
                   <h2>Projects</h2>
-                  <p>Here you can check out some of the projects I've completed</p>
+                  <p>Here you can check out some of the projects I've created</p>
 
                   <Tab.Container id="project-tabs" defaultActiveKey="first">
                     <Nav
@@ -60,26 +66,26 @@ export const Projects = () => {
                       id="pills-tab"
                     >
                       <Nav.Item>
-                        <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                        <Nav.Link eventKey="first">Frontend</Nav.Link>
                       </Nav.Item>
                       <Nav.Item>
-                        <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                        <Nav.Link eventKey="second">Backend</Nav.Link>
                       </Nav.Item>
                       <Nav.Item>
-                        <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                        <Nav.Link eventKey="third">Native</Nav.Link>
                       </Nav.Item>
                     </Nav>
                     <Tab.Content>
                       <Tab.Pane eventKey="first">
-                        Backend
+                        Frontend Applications
                         <Row>
                           {projects.map((project, index) => {
                             return <ProjectCard key={index} {...project} />;
                           })}
                         </Row>
                       </Tab.Pane>
-                      <Tab.Pane eventKey="second">Frontend</Tab.Pane>
-                      <Tab.Pane eventKey="third">Native</Tab.Pane>
+                      <Tab.Pane eventKey="second">Backend Applications</Tab.Pane>
+                      <Tab.Pane eventKey="third">Native Applications</Tab.Pane>
                     </Tab.Content>
                   </Tab.Container>
                 </div>
