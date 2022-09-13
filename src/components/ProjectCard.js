d45d39ffb46573ccd5a7f1841919aa0fc17ffb3e
TKTamir <104828119+TKTamir@users.noreply.github.com>
2022-09-13 22:23:56 +0200

    Add project-img class

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -4,7 +4,7 @@ export const ProjectCard = ({ title, description, imgUrl, appUrl }) => {
   return (
     <Col sm={6} md={4}>
       <div className="proj-imgbx">
-        <img src={imgUrl} alt="Project-Image" />
+        <img src={imgUrl} alt="Project-Image" className="project-img" />
 
         <div className="proj-txtx">
           <h4>{title}</h4>
